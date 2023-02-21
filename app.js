ee294353725110521d5e4c55531b37411086d478
bin2159 <bipinthms007@gmail.com>
2023-02-22 00:04:42 +0530

    Request / Response

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 const http=require('http')
 const server=http.createServer((req,res)=>{
-    console.log('Hello')
+    if(req.url=='/home'){
+        res.setHeader('Content-Type','text/html')
+        res.write('<html>')
+        res.write('<head><title>My first Page</title></head>')
+        res.write('<body><h1>Hello from Home page</></body>')
+        res.write('</html>')
+        res.end()
+    }
+    if(req.url=='/about'){
+        res.setHeader('Content-Type','text/html')
+        res.write('<html>')
+        res.write('<head><title>My first Page</title></head>')
+        res.write('<body><h1>Hello from About Us page</></body>')
+        res.write('</html>')
+        res.end()
+    }
+    if(req.url=='/node'){
+        res.setHeader('Content-Type','text/html')
+        res.write('<html>')
+        res.write('<head><title>My first Page</title></head>')
+        res.write('<body><h1>Hello from my node js project</></body>')
+        res.write('</html>')
+        res.end()
+    }
+    
 })
 server.listen(3000)
